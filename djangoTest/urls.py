48fcdcef7115f73a91e0accf8ac19437bf1607d6
cyla00 <ikhayam000@protonmail.com>
2020-11-06 16:05:17 +0100

    template folder with html view added

--- djangoTest/urls.py
+++ djangoTest/urls.py
@@ -22,5 +22,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('__debug__/', include(debug_toolbar.urls)),
-    path('', include('products.urls'))
+    path('products/', include('products.urls'))
 ]
--- products/views.py
+++ products/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def product_list(request):
-    return HttpResponse('./productsproduct_list.html')
+    return render(request, 'products/product_list.html')
